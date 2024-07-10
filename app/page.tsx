b281836da87761c3685f9229ee5437899f63f2b7
Roberto Himmelbauer <robertoh89@gmail.com>
2024-07-10 14:10:47 -0700

    Added Barchart

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
 import StatsCard from "@/components/StatsCard";
 import { Button } from "@/components/ui/button";
-import DashboardCardStats from "@/components/ui/DashboardCardStats";
+import DashboardCardStats from "@/components/DashboardCardStats";
 import Image from "next/image";
+import DashboardBarChart from "@/components/DashboardBarChart";
+import DashboardLastTransactions from "@/components/DashboardLastTransactions";
+import TransactionTable from "@/components/transaction-table/TransactionTable";
 
 export default function Home() {
   return (
@@ -10,10 +13,18 @@ export default function Home() {
         <div className="font-medium text-2xl">On-Demand Trading</div>
         <Button>Login</Button>
       </header>
-      <main className="flex min-h-screen flex-col px-12 pt-16 gap-5">
+      <main className="flex min-h-screen flex-col px-12 py-16 gap-5">
         <div className="text-4xl">Dashboard</div>
 
         <DashboardCardStats />
+        <div className="grid grid-cols-6 gap-3">
+          <div className="col-span-4">
+            <DashboardBarChart />
+          </div>
+          <div className="col-span-2">
+            <TransactionTable />
+          </div>
+        </div>
       </main>
     </div>
   );
